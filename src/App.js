c0ae9f038aac4a876b5fea8d9031172c4a16861a
franc <franc@LAPTOP-KM3FBE0P>
2024-09-23 15:14:08 +0800

    routing

--- src/App.js
+++ src/App.js
@@ -1,66 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { createContext, useContext, useState } from 'react';
-import { UserProvider } from './Contexts/UserContext';
-import Parent from './Components/Parent';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
-const UserContext = createContext();
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
+function About() {
+  return <h2>About Page</h2>;
+}
 
 function App() {
-  const [user, setUser] = useState('John Doe');
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Context State Management</h1>
-        <UserContext.Provider value={{ user, setUser }}>
-          <UserProfile />
-        </UserContext.Provider>
+        <h1>Routing</h1>
+        <Router>
+          <nav>
+            <Link to="/">Home</Link>
+            &nbsp;
+            <Link to="/about">About</Link>
+          </nav>
 
-        <br />
-        <br />
-        <h1>Context State Management (Separate Components)</h1>
-        <UserProvider>
-          <Parent />
-        </UserProvider>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
 }
 
-function UserProfile() {
-  const { user, setUser } = useContext(UserContext);
-
-  return (
-    <div>
-      <p>Username: {user}</p>
-      <button className='btn btn-primary' onClick={() => setUser('Jane Doe')}>Change Name</button>
-    </div>
-  );
-}
 
 export default App;
-
-
-/*
-function App() {
-  const user = 'John Doe';
-  return <Parent user={user} />;
-}
-
-function Parent({ user }) {
-  return <Child user={user} />;
-}
-
-function Child({ user }) {
-  return <GrandChild user={user} />;
-}
-
-function GrandChild({ user }) {
-  return <div>User: {user}</div>;
-}
-
-export default App;
-
-*/
